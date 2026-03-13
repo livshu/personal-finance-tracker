f3ff05ed772f2ed0d9e4c6066ae2c3016bd6477d
livshu <124776448+livshu@users.noreply.github.com>
2026-03-13 21:17:23 +0000

    Configure admin for finance models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Account, Category, Transaction
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("name", "account_type", "owner_type", "institution", "currency")
+    search_fields = ("name", "institution")
+    list_filter = ("account_type", "owner_type", "currency")
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "parent", "is_income")
+    search_fields = ("name",)
+    list_filter = ("is_income",)
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        "date",
+        "description_raw",
+        "account",
+        "category",
+        "amount",
+        "transaction_type",
+        "is_transfer",
+        "is_excluded",
+    )
+    search_fields = ("description_raw", "merchant_normalized", "source_file")
+    list_filter = (
+        "transaction_type",
+        "is_transfer",
+        "is_excluded",
+        "account",
+        "category",
+    )
+    date_hierarchy = "date"
